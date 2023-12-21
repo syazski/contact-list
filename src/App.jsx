9acea00a6e4827aaaa4b1a7c74906619b5a31627
Syaz S <syazski@gmail.com>
2023-12-20 20:29:46 -0600

    added table

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ContactList from './components/contactlist.jsx'
 
 function App() {
 
   return (
     <>
-      <div>
-      </div>
+      <ContactList />
     </>
   )
 }
